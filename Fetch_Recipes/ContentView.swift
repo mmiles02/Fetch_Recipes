5fd9fc3dc4f72533cba798c100a40c0af8811f99
Mitchell Miles <mitchell.miles02@gmail.com>
2024-07-02 21:41:14 -0500

    Testing 2

--- Fetch_Recipes/ContentView.swift
+++ Fetch_Recipes/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! It's Mitch?")
+            Text("Hello, world! It's Mitch!")
         }
         .padding()
     }

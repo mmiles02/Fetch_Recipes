b0819f9990aa96fc343f8a1d1faea6c6b814870e
Mitchell Miles <mitchell.miles02@gmail.com>
2024-07-02 21:33:52 -0500

    Initial Commit

---
+++ Fetch_Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch_Recipes
+//
+//  Created by Mitchell Miles on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch_Recipes/Fetch_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesApp.swift
+//  Fetch_Recipes
+//
+//  Created by Mitchell Miles on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
